494bafc95fa1c19bbf54ce7379e703fa61e0f46f
anikdutta90@gmail.com <anikdutta90@gmail.com>
2018-06-06 02:21:48 +0530

    Logger

--- src/micro-ui/src/store/store.js
+++ src/micro-ui/src/store/store.js
@@ -2,12 +2,13 @@ import {applyMiddleware, createStore, compose} from 'redux';
 import thunk from 'redux-thunk';
 import initialState from '../reducer/initialState'
 import rootReducer from '../reducer';
+import logger from 'redux-logger'
 
 
 const store= createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk,logger)
 );
 
 
