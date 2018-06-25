0358383e7e5dcd50ce5797837e6e891e2dc614f0
Soumit-B <soumitsince1989@gmail.com>
2018-06-25 22:05:19 +0530

    Changes

--- src/micro-ui/src/api/CourseApi.js
+++ src/micro-ui/src/api/CourseApi.js
@@ -3,7 +3,7 @@ import delay from './delay';
 // This file mocks a web API by working with the hard-coded data below.
 // It uses setTimeout to simulate the delay of an AJAX call.
 // All calls return promises.
-const courses = [
+/*const courses = [
     {
         id: "react-flux-building-applications",
         title: "Building Applications in React and Flux",
@@ -44,7 +44,205 @@ const courses = [
         length: "5:10",
         category: "HTML5"
     }
-];
+];*/
+
+const courses = [ {
+    "id" : 100101,
+    "iata" : "319",
+    "icao" : null,
+    "description" : "Airbus A319",
+    "type" : "Jet-engined aircraft",
+    "category" : "319"
+  }, {
+    "id" : 100201,
+    "iata" : "320",
+    "icao" : null,
+    "description" : "Airbus A320",
+    "type" : "Jet-engined aircraft",
+    "category" : "320"
+  }, {
+    "id" : 100301,
+    "iata" : "321",
+    "icao" : null,
+    "description" : "Airbus A321",
+    "type" : "Jet-engined aircraft",
+    "category" : "321"
+  }, {
+    "id" : 100401,
+    "iata" : "32S",
+    "icao" : null,
+    "description" : "Airbus A318 / A319 / A320 / A321",
+    "type" : "Jet-engined aircraft",
+    "category" : "32S"
+  }, {
+    "id" : 100501,
+    "iata" : "330",
+    "icao" : null,
+    "description" : "Airbus A330",
+    "type" : "Jet-engined aircraft",
+    "category" : "330"
+  }, {
+    "id" : 100601,
+    "iata" : "333",
+    "icao" : null,
+    "description" : "Airbus A330-300",
+    "type" : "Jet-engined aircraft",
+    "category" : "333"
+  }, {
+    "id" : 100701,
+    "iata" : "340",
+    "icao" : null,
+    "description" : "Airbus A340",
+    "type" : "Jet-engined aircraft",
+    "category" : "340"
+  }, {
+    "id" : 100801,
+    "iata" : "380",
+    "icao" : null,
+    "description" : "Airbus A380 Passenger",
+    "type" : "Jet-engined aircraft",
+    "category" : "380"
+  }, {
+    "id" : 100901,
+    "iata" : "388",
+    "icao" : null,
+    "description" : "Airbus A380-800 Passenger",
+    "type" : "Jet-engined aircraft",
+    "category" : "388"
+  }, {
+    "id" : 101001,
+    "iata" : "737",
+    "icao" : null,
+    "description" : "Boeing 737 Passenger",
+    "type" : "Jet-engined aircraft",
+    "category" : "737"
+  }, {
+    "id" : 101101,
+    "iata" : "747",
+    "icao" : null,
+    "description" : "Boeing 747 Passenger",
+    "type" : "Jet-engined aircraft",
+    "category" : "747"
+  }, {
+    "id" : 101201,
+    "iata" : "753",
+    "icao" : null,
+    "description" : "Boeing 757-300 Passenger",
+    "type" : "Jet-engined aircraft",
+    "category" : "753"
+  }, {
+    "id" : 101301,
+    "iata" : "757",
+    "icao" : null,
+    "description" : "Boeing 757 Passenger",
+    "type" : "Jet-engined aircraft",
+    "category" : "757"
+  }, {
+    "id" : 101401,
+    "iata" : "763",
+    "icao" : null,
+    "description" : "Boeing 767-300 Passenger",
+    "type" : "Jet-engined aircraft",
+    "category" : "763"
+  }, {
+    "id" : 101501,
+    "iata" : "764",
+    "icao" : null,
+    "description" : "Boeing 767-400 Passenger",
+    "type" : "Jet-engined aircraft",
+    "category" : "764"
+  }, {
+    "id" : 101601,
+    "iata" : "767",
+    "icao" : null,
+    "description" : "Boeing 767 Passenger",
+    "type" : "Jet-engined aircraft",
+    "category" : "767"
+  }, {
+    "id" : 101701,
+    "iata" : "76W",
+    "icao" : null,
+    "description" : "Boeing 767-300 (winglets) Passenger",
+    "type" : "Jet-engined aircraft",
+    "category" : "76W"
+  }, {
+    "id" : 101801,
+    "iata" : "773",
+    "icao" : null,
+    "description" : "Boeing 777-300",
+    "type" : "Jet-engined aircraft",
+    "category" : "773"
+  }, {
+    "id" : 101901,
+    "iata" : "777",
+    "icao" : null,
+    "description" : "Boeing 777",
+    "type" : "Jet-engined aircraft",
+    "category" : "777"
+  }, {
+    "id" : 102001,
+    "iata" : "77W",
+    "icao" : null,
+    "description" : "Boeing 777-300ER",
+    "type" : "Jet-engined aircraft",
+    "category" : "77W"
+  }, {
+    "id" : 102101,
+    "iata" : "787",
+    "icao" : null,
+    "description" : "Boeing 787",
+    "type" : "Jet-engined aircraft",
+    "category" : "787"
+  }, {
+    "id" : 102201,
+    "iata" : "788",
+    "icao" : null,
+    "description" : "Boeing 787-8",
+    "type" : "Jet-engined aircraft",
+    "category" : "788"
+  }, {
+    "id" : 102301,
+    "iata" : "ARJ",
+    "icao" : null,
+    "description" : "Avro RJ70 / RJ85 / RJ100",
+    "type" : "Jet-engined aircraft",
+    "category" : "ARJ"
+  }, {
+    "id" : 102401,
+    "iata" : "EMJ",
+    "icao" : null,
+    "description" : "Embraer 170 / 175 / 190 / 195",
+    "type" : "Jet-engined aircraft",
+    "category" : "EMJ"
+  }, {
+    "id" : 102501,
+    "iata" : "F70",
+    "icao" : null,
+    "description" : "Fokker 70",
+    "type" : "Jet-engined aircraft",
+    "category" : "F70"
+  }, {
+    "id" : 102601,
+    "iata" : "M80",
+    "icao" : null,
+    "description" : "Boeing (Douglas) MD-80",
+    "type" : "Jet-engined aircraft",
+    "category" : "M80"
+  }, {
+    "id" : 102701,
+    "iata" : "M83",
+    "icao" : null,
+    "description" : "Boeing (Douglas) MD-83",
+    "type" : "Jet-engined aircraft",
+    "category" : "M83"
+  }, {
+    "id" : 102801,
+    "iata" : "M88",
+    "icao" : null,
+    "description" : "Boeing (Douglas) MD-88",
+    "type" : "Jet-engined aircraft",
+    "category" : "M88"
+  } ]
 
 function replaceAll(str, find, replace) {
     return str.replace(new RegExp(find, 'g'), replace);
